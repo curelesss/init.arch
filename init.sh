0af9bc590d5fb807a16d79aec400d7c55fe39dfd
curelesss <curelesss@gmail.com>
2024-02-19 18:09:26 +0800

    script updated

--- init.sh
+++ init.sh
@@ -1,1 +1,17 @@
-ansible-playbook init.arch.book_-.yml --ask-become-pass -vv
+echo -*********************************-
+echo -** Run Ansible Init Play-book  **-
+echo -*********************************-
+
+ansible-playbook book_-.yml --ask-become-pass --ask-vault-pass -vv
+
+echo -*********************************-
+echo -** Set current repo to ssh     **-
+echo -*********************************-
+
+git remote set-url origin git@github.com:curelesss/init.ubuntu.git
+
+echo -*********************************-
+echo -** Testing Github Connection   **-
+echo -*********************************-
+
+ssh -T git@github.com
