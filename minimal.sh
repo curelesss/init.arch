490695c763b222cfd5fce069a93c2a7cc90d3c1b
curelesss <curelesss@gmail.com>
2024-02-21 11:05:10 +0800

    add ansible tasks for setup gnome desktop after minimal install arch

---
+++ minimal.sh
@@ -0,0 +1,6 @@
+echo -*********************************-
+echo -** Install Gnome DE            **-
+echo -*********************************-
+
+ansible-playbook minimal.book.yml --ask-become-pass -vv
+
